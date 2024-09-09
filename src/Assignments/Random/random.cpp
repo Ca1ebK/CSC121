bfffaf9101494f95ef56b45730bd336a059115c8
Caleb Kang <145171565+Ca1ebK@users.noreply.github.com>
2024-09-09 14:37:25 +0000

    Create a random even number between 10 and 12

--- src/Assignments/Random/random.cpp
+++ src/Assignments/Random/random.cpp
@@ -7,8 +7,8 @@ int main()
     // Set a random seed each time the program is re-compiled
     srand(static_cast<unsigned>(time(nullptr)));
 
-    // Prompt: generate an even random number between 10 and 20.
-    uint64_t randInt = rand() % 6 * 2 + 10;
+    // Prompt: generate an even random number between 10 and 12.
+    uint64_t randInt = rand() % 2 * 2 + 10;
     std::cout << randInt << std::endl; // print out the random integer.
 
     return 0;
