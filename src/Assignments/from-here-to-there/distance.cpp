1f9aecff8994cf2d8017d1e5314c637356922bbf
Caleb Kang <145171565+Ca1ebK@users.noreply.github.com>
2024-09-10 15:34:28 +0000

    3D Enhancements

--- src/Assignments/from-here-to-there/distance.cpp
+++ src/Assignments/from-here-to-there/distance.cpp
@@ -3,32 +3,60 @@
 
 int main()
 {
-    double x1;
-    double y1;
-
-    double x2;
-    double y2;
-
-    double distance;
 
     std::cout << "\nWelcome to the 2D Distance Program!\n" << std::endl;
 
-    std::cout << "What is the first point in the plane?" << std::endl;
+    std::cout << "Do you want to calculate a distance in 2 or 3 dimensions?" << std::endl;
 
-    std::cin >> x1 >> y1;
+    int numDimensions = 0;
+    std::cin >> numDimensions;
 
-    std::cout << "\nWhat is the second point in the plane?" << std::endl;
+    double x1 = 0.0;
+    double y1 = 0.0;
+    double z1 = 0.0;
 
-    std::cin >> x2 >> y2;
+    double x2 = 0.0;
+    double y2 = 0.0;
+    double z2 = 0.0;
 
-    std::cout << std::endl << "Calculating the distance now...\n" << std::endl;
+    double distance = 0.0;
 
-    distance = sqrt(pow(x2 - x1, 2.0) + pow(y2 - y1, 2.0));
+    if (numDimensions == 2)
+    {
+        std::cout << "What is the first point in the plane?" << std::endl;
 
-    std::cout << "The midpoint of the line segment between (" << x1 << ", " << y1 << ")"
+        std::cin >> x1 >> y1;
+
+        std::cout << "\nWhat is the second point in the plane?" << std::endl;
+
+        std::cin >> x2 >> y2;
+
+        std::cout << std::endl << "Calculating the distance now...\n" << std::endl;
+
+        distance = sqrt(pow(x2 - x1, 2.0) + pow(y2 - y1, 2.0));
+
+        std::cout << "The distance between (" << x1 << ", " << y1 << ")"
             " and (" << x2 << ", " << y2 << ") is " << distance << ".\n" << std::endl;
+    }
+    else if (numDimensions == 3)
+    {
+        std::cout << "What is the first point in the plane?" << std::endl;
 
-    std::cout << "Thank you for using the 2D Distance Program.\n" << std::endl;
+        std::cin >> x1 >> y1 >> z1;
+
+        std::cout << "\nWhat is the second point in the plane?" << std::endl;
+
+        std::cin >> x2 >> y2 >> z2;
+
+        std::cout << std::endl << "Calculating the distance now...\n" << std::endl;
+
+        distance = sqrt(pow(x2 - x1, 2.0) + pow(y2 - y1, 2.0) + pow(z2 - z1, 2.0));
+
+        std::cout << "The distance between (" << x1 << ", " << y1 << ", " << z1 << ")"
+            " and (" << x2 << ", " << y2 << ", " << z2 << ") is " << distance << ".\n" << std::endl;
+    }
+
+    std::cout << "Thank you for using the Distance Program.\n" << std::endl;
 
     return 0;
 }
