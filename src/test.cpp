4a3cc1072a29c079ec9b1a323da89ac1d99bc756
Caleb Kang <145171565+Ca1ebK@users.noreply.github.com>
2024-09-16 14:47:00 +0000

    Test

---
+++ src/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    for (int loop = 0; loop < 10; loop++)
+    {
+        for (loop = 0; loop < 10; loop++)
+        {
+            ;
+        }
+        std::cout << loop << std::endl;
+    }
+}
